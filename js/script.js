84b00263cab1ba5041edf78cb266240c03ca3158
mludwig99 <michalludwig99@gmail.com>
2022-09-13 17:04:13 +0200

    Finall-project

--- js/script.js
+++ js/script.js
@@ -7,23 +7,26 @@ const inputName = document.querySelector("#name");
 const inputAmount = document.querySelector("#amount");
 const selectCategory = document.querySelector("#category");
 
+const styleBacgroundLight = document.querySelector('.light');
+const styleBacgroundDark = document.querySelector('.dark');
+
 const transactionBtn = document.querySelector(".add-transaction");
 const saveBtn = document.querySelector(".save");
 const cancelBtn = document.querySelector(".cancel");
 // const deleteBtn = document.querySelector(".delete");
-const dleteAllBtn = document.querySelector(".delete-all");
+const deleteAllBtn = document.querySelector(".delete-all");
 
-let root = document.documentElement;
+
 let ID = 0;
 let categoryIcon;
 let selectedCategory;
 let moneyArr = [0];
 
 const showPanel = () => {
-  transactionPanel.style.display = 'flex';
+  transactionPanel.style.display = "flex";
 };
 const closePanel = () => {
-  transactionPanel.style.display = 'none';
+  transactionPanel.style.display = "none";
   clearInputs();
 };
 
@@ -31,11 +34,9 @@ const checkForm = () => {
   if (
     inputName.value !== "" &&
     inputAmount.value !== "" &&
-      selectCategory.value !== "none"
-      
+    selectCategory.value !== "none"
   ) {
     createNewTransaction();
-      
   } else {
     alert("Some input data is not entered!");
   }
@@ -51,9 +52,9 @@ const createNewTransaction = () => {
   const newTransaction = document.createElement("div");
 
   newTransaction.classList.add("transaction");
-    newTransaction.setAttribute("id", ID);
+  newTransaction.setAttribute("id", ID);
 
-    checkCategory(selectedCategory);
+  checkCategory(selectedCategory);
 
   newTransaction.innerHTML = `
         <p class="transaction-name">
@@ -71,18 +72,16 @@ const createNewTransaction = () => {
       newTransaction.classList.add("income")
     : sectionExpenses.appendChild(newTransaction) &&
       newTransaction.classList.add("expense");
-    moneyArr.push(parseFloat(inputAmount.value));
-    
-    closePanel();
-    ID++;
-    clearInputs();
+  moneyArr.push(parseFloat(inputAmount.value));
+  countMoney(moneyArr);
+  closePanel();
+  ID++;
+  clearInputs();
 };
 
 const selectCat = () => {
-    selectedCategory =
-      selectCategory.options[selectCategory.selectedIndex].text;
-}
-
+  selectedCategory = selectCategory.options[selectCategory.selectedIndex].text;
+};
 
 const checkCategory = (transaction) => {
   switch (transaction) {
@@ -101,6 +100,47 @@ const checkCategory = (transaction) => {
   }
 };
 
+const countMoney = (money) => {
+  const newMoney = money.reduce((a, b) => a + b);
+  availableMoney.textContent = `${newMoney}zł`;
+};
+
+const deleteTransaction = (id) => {
+  const transactionToDelete = document.getElementById(id);
+
+  const transactionAmount = parseFloat(
+    transactionToDelete.childNodes[3].innerText
+  );
+
+  const indexOfTransaction = moneyArr.indexOf(transactionAmount);
+  moneyArr.splice(indexOfTransaction, 1);
+  transactionToDelete.classList.contains("income")
+    ? sectionIncome.removeChild(transactionToDelete)
+    : sectionExpenses.removeChild(transactionToDelete);
+  countMoney(moneyArr);
+};
+
+const deleteAllTransactions = () => {
+  sectionIncome.innerHTML = " <h3>Money Income</h3>";
+  sectionExpenses.innerHTML = "<h3>Expenses:</h3>";
+  availableMoney.textContent = "0zł";
+  moneyArr = [0];
+};
+
+
+const changeStyleToLight = () => {
+  document.body.className = '';
+  
+};
+const changeStyleToDark = () => {
+  document.body.className = "dark-bacground";
+  // body.style.setProperty("--bacground-dark-color", 'rgb(128, 128, 128, 0.5)');
+}
+
 transactionBtn.addEventListener("click", showPanel);
 cancelBtn.addEventListener("click", closePanel);
-saveBtn.addEventListener("click", checkForm, );
+saveBtn.addEventListener("click", checkForm);
+deleteAllBtn.addEventListener("click", deleteAllTransactions);
+styleBacgroundLight.addEventListener("click", changeStyleToLight);
+styleBacgroundDark.addEventListener("click", changeStyleToDark);
+
